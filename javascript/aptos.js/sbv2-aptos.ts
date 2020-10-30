9e15022fcd49382537167ca485d14813c42665b5
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2020-10-30 14:59:57 +0000

    Bump next snapshot (#5579)

--- javascript/aptos.js/sbv2-aptos.ts
+++ javascript/aptos.js/sbv2-aptos.ts
@@ -5,4 +5,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
-// import yargs f
+// import yargs from "yargs";
+// import { hideBin } from "yargs/helpers";
+import * as YAML from "yaml";
+import * as fs from "fs";
+import * as path from "path";
+
+import chalk from "chalk";
+import { AptosAccount, AptosClient, FaucetClient, HexString } from "aptos";
+import {
+  AggregatorAccount,
+  JobAccount,
+  OracleAccount,
+  OracleQueueAccount,
+  StateAccount,
+  CrankAccount,
+  SWITCHBOARD_DEVNET_ADDRESS,
+} from "./src";
+import { OracleJob } from "@switchboard-xyz/common";
+
+export const CHECK_ICON = chalk.green("\u2714");
+export

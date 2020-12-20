b594976c396702fd2e30ff974ba51f15d62926ab
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2020-12-20 06:37:49 +0000

    Fix linting.

--- javascript/aptos.js/scripts/deploy_switchboard_feeds_prod.ts
+++ javascript/aptos.js/scripts/deploy_switchboard_feeds_prod.ts
@@ -1,1 +1,24 @@
-imp
+import { AptosClient, AptosAccount, FaucetClient, HexString } from "aptos";
+import {
+  generateResourceAccountAddress,
+  bcsAddressToBytes,
+  createFeed,
+} from "../lib/cjs";
+import * as YAML from "yaml";
+import * as fs from "fs";
+import Big from "big.js";
+import { aptBinance } from "./job_data/apt";
+import {
+  btcBinance,
+  btcBitfinex,
+  btcCoinbase,
+  btcFtx,
+  btcKraken,
+} from "./job_data/btc";
+import {
+  ethBinance,
+  ethBitfinex,
+  ethCoinbase,
+  ethFtx,
+  ethKraken,
+} from "./job_data/e

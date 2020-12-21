fb40651e2954ae36d369b26855a188ec43f67e36
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2020-12-21 09:29:21 +0000

    [ZF-10889] Update all copyright headers

--- javascript/aptos.js/scripts/deploy_switchboard_feeds_prod.ts
+++ javascript/aptos.js/scripts/deploy_switchboard_feeds_prod.ts
@@ -21,4 +21,25 @@ import {
   ethCoinbase,
   ethFtx,
   ethKraken,
-} from "./job_data/e
+} from "./job_data/eth";
+import {
+  nearBinance,
+  nearBitfinex,
+  nearCoinbase,
+  nearFtx,
+} from "./job_data/near";
+import { solBinance, solBitfinex, solFtx, solFtxus } from "./job_data/sol";
+import {
+  usdcBinance,
+  usdcBitstamp,
+  usdcBittrex,
+  usdcKraken,
+} from "./job_data/usdc";
+
+const NODE_URL = "https://aptos-mainnet.nodereal.io/v1/baee52f0ce4f4dd0893fd6466659bd04/v1";
+
+const SWITCHBOARD_ADDRESS =
+  "0x7d7e436f0b2aafde60774efb26ccc432cf881b677aca7faaf2a01879bd19fb8";
+
+const SWITCHBOARD_QUEUE_ADDRESS =
+  "0x11fb

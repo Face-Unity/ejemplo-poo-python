c18f7f9d466796125f141433d23520c6d2ce8d76
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-01-16 20:36:02 +0000

    Files added

--- javascript/aptos.js/scripts/extend_and_push_feeds_to_crank.ts
+++ javascript/aptos.js/scripts/extend_and_push_feeds_to_crank.ts
@@ -22,4 +22,20 @@ const SWITCHBOARD_ADDRESS =
 // const QUEUE_ADDRESS =
 // "0x11fbd91e4a718066891f37958f0b68d10e720f2edf8d57854fb20c299a119a8c";
 const QUEUE_ADDRESS =
-  "0xc887072e37f17f9cc7afc0a00e2b
+  "0xc887072e37f17f9cc7afc0a00e2b283775d703c610acca3997cb26e74bc53f3b";
+
+const feeds = [
+  "0xdc7f6fbc4efe2995e1e37b9f73d113085e4ee3597d47210a2933ad3bf5b78774",
+  "0x7b5f536d201280a10d33d8c2202a1892b1dd8247aecfef7762ea8e7565eac7b6",
+  "0x5af65afeeab555f8b742ce7fc2c539a5cb6a6fb2a6e6d96bc1b075fb28067808",
+  "0xdc1045b4d9fd1f4221fc2f91b2090d88483ba9745f29cf2d96574611204659a5",
+];
+
+(async () => {
+  const client = new AptosClient(NODE_URL);
+
+  let funder;
+
+  // if file extension ends with yaml
+  try {
+    const parsedYaml = YAML.par

2b14d4c3628544518f0de29799f4394f9c41ee1e
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-01-28 08:13:27 +0000

    ginas project is renamed to DebOps project

--- javascript/aptos.js/src/generated/errors.ts
+++ javascript/aptos.js/src/generated/errors.ts
@@ -1,1 +1,15 @@
-export enum Sw
+export enum SwitchboardErrorEnum {
+  Generic = "Generic",
+  StateNotFound = "StateNotFound",
+  QueueNotFound = "QueueNotFound",
+  OracleNotFound = "OracleNotFound",
+  JobNotFound = "JobNotFound",
+  CrankNotFound = "CrankNotFound",
+  AggregatorNotFound = "AggregatorNotFound",
+  LeaseNotFound = "LeaseNotFound",
+  OracleWalletNotFound = "OracleWalletNotFound",
+  StateAlreadyExists = "StateAlreadyExists",
+  QueueAlreadyExists = "QueueAlreadyExists",
+  OracleAlreadyExists = "OracleAlreadyExists",
+  JobAlreadyExists = "JobAlreadyExists",
+  CrankAlrea

41c15829ac73c112a0682455dae46733fc91cf76
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-03-22 20:55:13 +0000

    bnx2x: fix possible panic under memory stress

--- javascript/aptos.js/src/generated/types/AggregatorHistoryData.ts
+++ javascript/aptos.js/src/generated/types/AggregatorHistoryData.ts
@@ -1,4 +1,22 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import BN from "bn.js"; // eslint-disable-line @typescript-
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IAggregatorHistoryData {
+  history: Array<types.AggregatorHistoryRow>;
+  historyWriteIdx: BN;
+}
+
+export interface AggregatorHistoryDataJSON {
+  history: Array<types.AggregatorHistoryRowJSON>;
+  historyWriteIdx: string;
+}
+
+export interface AggregatorHistoryDataMoveStruct {
+  history: Array<types.AggregatorHistoryRowMoveStruct>;
+  history_write_idx: string;
+}
+
+export class AggregatorHistoryData implements IAggregatorHistoryData {
+  readonly history: 

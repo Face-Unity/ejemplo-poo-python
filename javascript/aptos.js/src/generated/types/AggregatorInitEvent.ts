ddf4ba8f4e50cd8a59b177cceda0a518b99290b1
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-03-30 14:11:18 +0000

    Upgrade to bundles-pom 12

--- javascript/aptos.js/src/generated/types/AggregatorInitEvent.ts
+++ javascript/aptos.js/src/generated/types/AggregatorInitEvent.ts
@@ -1,3 +1,19 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-import { HexString } from "aptos"; // esl
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IAggregatorInitEvent {
+  aggregatorAddress: HexString;
+}
+
+export interface AggregatorInitEventJSON {
+  aggregatorAddress: string;
+}
+
+export interface AggregatorInitEventMoveStruct {
+  aggregator_address: string;
+}
+
+export class AggregatorInitEvent implements IAggregatorInitEvent {
+  readonly aggregatorAddress: HexString;

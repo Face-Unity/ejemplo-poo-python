42c76aff81dbe31448dbcfe498ca98b392a032fc
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-04-03 15:39:06 +0000

    added commenting

--- javascript/aptos.js/src/generated/types/AggregatorJobData.ts
+++ javascript/aptos.js/src/generated/types/AggregatorJobData.ts
@@ -4,4 +4,27 @@ import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export interface IAggregatorJobData {
-  jobKeys: Array<H
+  jobKeys: Array<HexString>;
+  jobWeights: Uint8Array;
+  jobsChecksum: Uint8Array;
+}
+
+export interface AggregatorJobDataJSON {
+  jobKeys: Array<string>;
+  jobWeights: Array<number>;
+  jobsChecksum: Array<number>;
+}
+
+export interface AggregatorJobDataMoveStruct {
+  job_keys: Array<string>;
+  job_weights: string;
+  jobs_checksum: string;
+}
+
+export class AggregatorJobData implements IAggregatorJobData {
+  readonly jobKeys: Array<HexString>;
+  readonly jobWeights: Uint8Array;
+  readonly jobsChecksum: Uint8Array;
+
+  constructor(fields: IAggregatorJobData) {
+    this.j

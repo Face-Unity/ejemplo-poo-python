eefd78591a8f4a791ef7871bd2bc95b0f24e45bb
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-04-12 12:11:10 +0000

    Schema update

--- javascript/aptos.js/src/generated/types/AggregatorLockParams.ts
+++ javascript/aptos.js/src/generated/types/AggregatorLockParams.ts
@@ -1,3 +1,25 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-import { HexString } from "aptos"; // eslint-disable-line @ty
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IAggregatorLockParams {
+  aggregatorAddr: HexString;
+}
+
+export interface AggregatorLockParamsJSON {
+  aggregatorAddr: string;
+}
+
+export interface AggregatorLockParamsMoveStruct {
+  aggregator_addr: string;
+}
+
+export class AggregatorLockParams implements IAggregatorLockParams {
+  readonly aggregatorAddr: HexString;
+
+  constructor(fields: IAggregatorLockParams) {
+    this.aggregatorAddr = fields.aggregatorAddr;
+  }
+
+ 

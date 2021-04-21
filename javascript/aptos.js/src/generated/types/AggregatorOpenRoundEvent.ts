2de930fea5035dccb7bf41f13e3ee9056f297125
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-04-21 07:54:42 +0000

    Update CONTRIBUTORS

--- javascript/aptos.js/src/generated/types/AggregatorOpenRoundEvent.ts
+++ javascript/aptos.js/src/generated/types/AggregatorOpenRoundEvent.ts
@@ -1,4 +1,24 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import BN from
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IAggregatorOpenRoundEvent {
+  aggregatorAddress: HexString;
+  oracleKeys: Array<HexString>;
+  jobKeys: Array<HexString>;
+}
+
+export interface AggregatorOpenRoundEventJSON {
+  aggregatorAddress: string;
+  oracleKeys: Array<string>;
+  jobKeys: Array<string>;
+}
+
+export interface AggregatorOpenRoundEventMoveStruct {
+  aggregator_address: string;
+  oracle_keys: Array<string>;
+  job_keys: Array<string>;
+}
+
+export class AggregatorOpenRoundEvent implements IAggregat

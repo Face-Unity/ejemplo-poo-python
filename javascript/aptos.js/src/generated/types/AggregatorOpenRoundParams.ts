1309e438ef0d6771ff2dec6bbfd05e9c8a84adfc
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-04-23 14:07:57 +0000

    deploy from travis

--- javascript/aptos.js/src/generated/types/AggregatorOpenRoundParams.ts
+++ javascript/aptos.js/src/generated/types/AggregatorOpenRoundParams.ts
@@ -1,1 +1,16 @@
-import * as types fr
+import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IAggregatorOpenRoundParams {
+  aggregatorAddr: HexString;
+  jitter: BN;
+}
+
+export interface AggregatorOpenRoundParamsJSON {
+  aggregatorAddr: string;
+  jitter: string;
+}
+
+export interface AggregatorOpenRoundParamsMov

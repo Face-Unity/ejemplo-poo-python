08f73902fee06c47478c1e27ae726d0f1359197e
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-04-29 08:37:29 +0000

    ALSA: timer: Fix leak in SNDRV_TIMER_IOCTL_PARAMS

--- javascript/aptos.js/src/generated/types/AggregatorResultsConfig.ts
+++ javascript/aptos.js/src/generated/types/AggregatorResultsConfig.ts
@@ -4,4 +4,20 @@ import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export interface IAggregatorResultsConfig {
-  varianc
+  varianceThreshold: types.SwitchboardDecimal;
+  forceReportPeriod: BN;
+  minJobResults: BN;
+  expiration: BN;
+}
+
+export interface AggregatorResultsConfigJSON {
+  varianceThreshold: types.SwitchboardDecimalJSON;
+  forceReportPeriod: string;
+  minJobResults: string;
+  expiration: string;
+}
+
+export interface AggregatorResultsConfigMoveStruct {
+  variance_threshold: types.SwitchboardDecimalMoveStruct;
+  force_report_period: string;
+  min_job_results:

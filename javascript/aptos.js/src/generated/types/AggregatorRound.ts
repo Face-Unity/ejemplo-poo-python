32a83c4f2c5de6366e5ffbed2bbe38868b6edcc6
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-05-04 12:45:08 +0000

    Scrutinizer fixes

--- javascript/aptos.js/src/generated/types/AggregatorRound.ts
+++ javascript/aptos.js/src/generated/types/AggregatorRound.ts
@@ -1,4 +1,20 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import BN from "bn.js"; // esl
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IAggregatorRound {
+  id: BN;
+  roundOpenTimestamp: BN;
+  roundOpenBlockHeight: BN;
+  result: types.SwitchboardDecimal;
+  stdDeviation: types.SwitchboardDecimal;
+  minResponse: types.SwitchboardDecimal;
+  maxResponse: types.SwitchboardDecimal;
+  oracleKeys: Array<HexString>;
+  medians: Array<types.SwitchboardDecimal | undefined>;
+  currentPayout: Array<types.SwitchboardDecimal>;
+  errorsFulfilled: Array<boolean>;
+  numSuccess: BN;
+  numError: BN;
+  isC

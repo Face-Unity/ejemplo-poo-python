dee0324d40e83777b8cc3046169e1432c6a2240f
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-05-12 20:06:10 +0000

    Update FASTA

--- javascript/aptos.js/src/generated/types/AggregatorSaveResultEvent.ts
+++ javascript/aptos.js/src/generated/types/AggregatorSaveResultEvent.ts
@@ -1,1 +1,14 @@
-i
+import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IAggregatorSaveResultEvent {
+  aggregatorAddress: HexString;
+  oracleKey: HexString;
+  value: types.SwitchboardDecimal;
+}
+
+export interface AggregatorSaveResultEventJSON {
+  aggregatorAddress: string;
+  oracleKey: 

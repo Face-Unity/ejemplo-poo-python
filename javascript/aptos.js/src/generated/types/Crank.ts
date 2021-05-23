738cc89ab0e979a9f01911424dea4549bfb4128d
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-05-23 14:55:46 +0000

    Актуализация

--- javascript/aptos.js/src/generated/types/Crank.ts
+++ javascript/aptos.js/src/generated/types/Crank.ts
@@ -1,4 +1,27 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import BN from "bn
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface ICrank {
+  heap: Array<types.CrankRow>;
+  queueAddr: HexString;
+  createdAt: BN;
+  jitterModifier: BN;
+  features: Array<boolean>;
+  _ebuf: Uint8Array;
+}
+
+export interface CrankJSON {
+  heap: Array<types.CrankRowJSON>;
+  queueAddr: string;
+  createdAt: string;
+  jitterModifier: string;
+  features: Array<boolean>;
+  _ebuf: Array<number>;
+}
+
+export interface CrankMoveStruct {
+  heap: Array<types.CrankRowMoveStruct>;
+  queue_addr: string;
+  

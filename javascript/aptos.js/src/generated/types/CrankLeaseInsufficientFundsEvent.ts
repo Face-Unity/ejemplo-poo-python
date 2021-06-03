f48ebacf5bd91be874481fd608238e0434e38285
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-03 11:05:30 +0000

    fixes for windows

--- javascript/aptos.js/src/generated/types/CrankLeaseInsufficientFundsEvent.ts
+++ javascript/aptos.js/src/generated/types/CrankLeaseInsufficientFundsEvent.ts
@@ -1,4 +1,23 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import BN from "bn.js"; // eslint-disa
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface ICrankLeaseInsufficientFundsEvent {
+  aggregatorAddress: HexString;
+}
+
+export interface CrankLeaseInsufficientFundsEventJSON {
+  aggregatorAddress: string;
+}
+
+export interface CrankLeaseInsufficientFundsEventMoveStruct {
+  aggregator_address: string;
+}
+
+export class CrankLeaseInsufficientFundsEvent
+  implements ICrankLeaseInsufficientFundsEvent
+{
+  readonly aggregatorAddress: HexString;
+
+  constructor(fields: ICrankLeaseInsufficie

0e00478c7a84ea2af210c1d43866cab3302bf3e3
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-06 10:05:52 +0000

    modify posts

--- javascript/aptos.js/src/generated/types/CrankPopExpectedFailureEvent.ts
+++ javascript/aptos.js/src/generated/types/CrankPopExpectedFailureEvent.ts
@@ -3,4 +3,22 @@ import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-e
+export interface ICrankPopExpectedFailureEvent {
+  aggregatorAddress: HexString;
+}
+
+export interface CrankPopExpectedFailureEventJSON {
+  aggregatorAddress: string;
+}
+
+export interface CrankPopExpectedFailureEventMoveStruct {
+  aggregator_address: string;
+}
+
+export class CrankPopExpectedFailureEvent
+  implements ICrankPopExpectedFailureEvent
+{
+  readonly aggregatorAddress: HexString;
+
+  constructor(fields: ICrankPopExpectedFailureEvent) {
+    this.aggregatorAddress = fields.aggre

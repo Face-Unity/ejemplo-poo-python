a6b1350e731a4465ad6af488694734d228b744b8
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-09 17:28:35 +0000

    Update resource

--- javascript/aptos.js/src/generated/types/CrankPushParams.ts
+++ javascript/aptos.js/src/generated/types/CrankPushParams.ts
@@ -3,4 +3,25 @@ import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-export
+export interface ICrankPushParams {
+  crankAddr: HexString;
+  aggregatorAddr: HexString;
+}
+
+export interface CrankPushParamsJSON {
+  crankAddr: string;
+  aggregatorAddr: string;
+}
+
+export interface CrankPushParamsMoveStruct {
+  crank_addr: string;
+  aggregator_addr: string;
+}
+
+export class CrankPushParams implements ICrankPushParams {
+  readonly crankAddr: HexString;
+  readonly aggregatorAddr: HexString;
+
+  constructor(fields: ICrankPushParams) {
+    this.crankAddr = fields.crankAddr;
+    this.aggregatorAddr = fields.aggregator

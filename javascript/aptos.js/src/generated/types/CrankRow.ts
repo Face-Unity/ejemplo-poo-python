fc99a652302946d0d7a995bb345b4b518ece2373
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-10 08:00:07 +0000

    correct readme

--- javascript/aptos.js/src/generated/types/CrankRow.ts
+++ javascript/aptos.js/src/generated/types/CrankRow.ts
@@ -3,4 +3,28 @@ import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-export interface ICrankRow 
+export interface ICrankRow {
+  aggregatorAddr: HexString;
+  timestamp: BN;
+}
+
+export interface CrankRowJSON {
+  aggregatorAddr: string;
+  timestamp: string;
+}
+
+export interface CrankRowMoveStruct {
+  aggregator_addr: string;
+  timestamp: string;
+}
+
+export class CrankRow implements ICrankRow {
+  readonly aggregatorAddr: HexString;
+  readonly timestamp: BN;
+
+  constructor(fields: ICrankRow) {
+    this.aggregatorAddr = fields.aggregatorAddr;
+    this.timestamp = fields.timestamp;
+  }
+
+  toJSON(): CrankRo

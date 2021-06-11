6739b65bc23a2e0b0dd1dd14c842395f33d84d99
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-11 10:03:11 +0000

    Updated SQL

--- javascript/aptos.js/src/generated/types/EscrowManagerItem.ts
+++ javascript/aptos.js/src/generated/types/EscrowManagerItem.ts
@@ -1,4 +1,27 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import BN 
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IEscrowManagerItem {
+  handle: HexString;
+}
+
+export interface EscrowManagerItemJSON {
+  handle: string;
+}
+
+export interface EscrowManagerItemMoveStruct {
+  handle: string;
+}
+
+export class EscrowManagerItem implements IEscrowManagerItem {
+  readonly handle: HexString;
+
+  constructor(fields: IEscrowManagerItem) {
+    this.handle = fields.handle;
+  }
+
+  toJSON(): EscrowManagerItemJSON {
+    return {
+      handl

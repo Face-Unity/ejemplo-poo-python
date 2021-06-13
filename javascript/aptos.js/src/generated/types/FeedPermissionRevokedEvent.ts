c61d3cca558817d6dc5f7fb561999cb62fe4be9e
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-13 12:30:40 +0000

    Added my name

--- javascript/aptos.js/src/generated/types/FeedPermissionRevokedEvent.ts
+++ javascript/aptos.js/src/generated/types/FeedPermissionRevokedEvent.ts
@@ -1,1 +1,18 @@
-import * as types from "./"; // esl
+import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IFeedPermissionRevokedEvent {
+  aggregatorAddress: HexString;
+  timestamp: BN;
+}
+
+export interface FeedPermissionRevokedEventJSON {
+  aggregatorAddress: string;
+  timestamp: string;
+}
+
+export interface FeedPermissionRevokedEventMoveStruct {
+  aggregator_address: string;
+  timestamp: string;

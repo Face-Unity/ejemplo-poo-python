f87565fb67578159022c5b7157c6bcdc1a13485b
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-19 09:14:47 +0000

    Update lab05.md

--- javascript/aptos.js/src/generated/types/Job.ts
+++ javascript/aptos.js/src/generated/types/Job.ts
@@ -5,4 +5,30 @@ import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export interface IJob {
   addr: HexString;
-  name: Uin
+  name: Uint8Array;
+  metadata: Uint8Array;
+  authority: HexString;
+  expiration: BN;
+  hash: Uint8Array;
+  data: Uint8Array;
+  referenceCount: BN;
+  totalSpent: BN;
+  createdAt: BN;
+  variables: Array<Uint8Array>;
+  features: Array<boolean>;
+  _ebuf: Uint8Array;
+}
+
+export interface JobJSON {
+  addr: string;
+  name: Array<number>;
+  metadata: Array<number>;
+  authority: string;
+  expiration: string;
+  hash: Array<number>;
+  data: Array<number>;
+  referenceCount: string;
+  totalSpent: string;
+  createdAt: string;
+  variables: Array<Array<number>>;
+  f

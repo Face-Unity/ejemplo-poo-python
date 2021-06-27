4969d4618b862ba5b16957131d5daeefe0d6b0bb
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-27 06:22:22 +0000

    add links

--- javascript/aptos.js/src/generated/types/JobConfigParams.ts
+++ javascript/aptos.js/src/generated/types/JobConfigParams.ts
@@ -1,1 +1,17 @@
-import *
+import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IJobConfigParams {
+  name: Uint8Array;
+  metadata: Uint8Array;
+  authority: HexString;
+  data: Uint8Array;
+}
+
+export interface JobConfigParamsJSON {
+  name: Array<number>;
+  metadata: Array<number>;
+  authority: string;
+  data: Array

699032c6ed2af174b18998ac0537096ab5c5c90d
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-29 18:04:51 +0000

    strings

--- javascript/aptos.js/src/generated/types/LeaseExtendParams.ts
+++ javascript/aptos.js/src/generated/types/LeaseExtendParams.ts
@@ -1,3 +1,23 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-import { HexString } from "aptos"; // eslint-disable-line @types
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface ILeaseExtendParams {
+  aggregatorAddr: HexString;
+  loadAmount: BN;
+}
+
+export interface LeaseExtendParamsJSON {
+  aggregatorAddr: string;
+  loadAmount: string;
+}
+
+export interface LeaseExtendParamsMoveStruct {
+  aggregator_addr: string;
+  load_amount: string;
+}
+
+export class LeaseExtendParams implements ILeaseExtendParams {
+  readonly aggregatorAddr: HexString;
+  readonly loadAmount: BN

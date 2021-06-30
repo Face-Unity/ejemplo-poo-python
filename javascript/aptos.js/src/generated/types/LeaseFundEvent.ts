79d84ecbee5bd3f3749e25ce0ed89b1071855a68
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-06-30 18:09:33 +0000

    map fix

--- javascript/aptos.js/src/generated/types/LeaseFundEvent.ts
+++ javascript/aptos.js/src/generated/types/LeaseFundEvent.ts
@@ -1,3 +1,27 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-import { HexString } from "aptos"; // eslint-dis
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface ILeaseFundEvent {
+  leaseAddress: HexString;
+  funder: HexString;
+  amount: BN;
+  timestamp: BN;
+}
+
+export interface LeaseFundEventJSON {
+  leaseAddress: string;
+  funder: string;
+  amount: string;
+  timestamp: string;
+}
+
+export interface LeaseFundEventMoveStruct {
+  lease_address: string;
+  funder: string;
+  amount: string;
+  timestamp: string;
+}
+
+export class LeaseFundEvent implemen

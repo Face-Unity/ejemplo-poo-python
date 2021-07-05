54d922b955e9ed110fd005a51f466827012699ff
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-07-05 20:34:42 +0000

    HID: fix a couple of off-by-ones

--- javascript/aptos.js/src/generated/types/LeaseWithdrawEvent.ts
+++ javascript/aptos.js/src/generated/types/LeaseWithdrawEvent.ts
@@ -1,1 +1,19 @@
-import * as types from "./"; // eslint-disable-line @typescript-
+import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface ILeaseWithdrawEvent {
+  leaseAddress: HexString;
+  destinationWallet: HexString;
+  previousAmount: BN;
+  newAmount: BN;
+  timestamp: BN;
+}
+
+export interface LeaseWithdrawEventJSON {
+  leaseAddress: string;
+  destinationWallet: string;
+  previousAmount: string;
+  newAmount: string;
+  timestamp:

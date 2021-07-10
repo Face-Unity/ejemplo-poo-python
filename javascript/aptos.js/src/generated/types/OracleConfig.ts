f8046ebfcdd15a3c700984d7f880de662693d387
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-07-10 18:39:47 +0000

    updated comments

--- javascript/aptos.js/src/generated/types/OracleConfig.ts
+++ javascript/aptos.js/src/generated/types/OracleConfig.ts
@@ -1,3 +1,26 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-import { HexString } from "aptos"; // eslint-disable-line @typescript-eslin
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IOracleConfig {
+  authority: HexString;
+  queueAddr: HexString;
+}
+
+export interface OracleConfigJSON {
+  authority: string;
+  queueAddr: string;
+}
+
+export interface OracleConfigMoveStruct {
+  authority: string;
+  queue_addr: string;
+}
+
+export class OracleConfig implements IOracleConfig {
+  readonly authority: HexString;
+  readonly queueAddr: HexString;
+
+  constructor(fields: IOracleConfig) {
+    this.authority = fields.authority

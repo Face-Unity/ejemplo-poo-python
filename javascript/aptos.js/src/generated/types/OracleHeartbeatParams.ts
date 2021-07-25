43aef98547073b4157fbb8b49003368ee746aec7
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-07-25 21:46:59 +0000

    add main field to package.json

--- javascript/aptos.js/src/generated/types/OracleHeartbeatParams.ts
+++ javascript/aptos.js/src/generated/types/OracleHeartbeatParams.ts
@@ -1,1 +1,21 @@
-import * as types from "./"; // eslint-disable-line @typescript-e
+import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IOracleHeartbeatParams {
+  oracleAddr: HexString;
+}
+
+export interface OracleHeartbeatParamsJSON {
+  oracleAddr: string;
+}
+
+export interface OracleHeartbeatParamsMoveStruct {
+  oracle_addr: string;
+}
+
+export class OracleHeartbeatParams implements IOracleHeartbeatParams {
+  readonly oracleAddr: HexString;
+
+  constructor(f

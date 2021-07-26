2ee5aa40061b56547a412e2bd02db7835affaf80
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-07-26 15:50:47 +0000

    update Tbody

--- javascript/aptos.js/src/generated/types/OracleHeartbeatParams.ts
+++ javascript/aptos.js/src/generated/types/OracleHeartbeatParams.ts
@@ -18,4 +18,27 @@ export interface OracleHeartbeatParamsMoveStruct {
 export class OracleHeartbeatParams implements IOracleHeartbeatParams {
   readonly oracleAddr: HexString;
 
-  constructor(f
+  constructor(fields: IOracleHeartbeatParams) {
+    this.oracleAddr = fields.oracleAddr;
+  }
+
+  toJSON(): OracleHeartbeatParamsJSON {
+    return {
+      oracleAddr: this.oracleAddr.toString(),
+    };
+  }
+
+  static fromJSON(obj: OracleHeartbeatParamsJSON) {
+    return new OracleHeartbeatParams({
+      oracleAddr: HexString.ensure(obj.oracleAddr),
+    });
+  }
+
+  toMoveStruct(): OracleHeartbeatParamsMoveStruct {
+    return {
+      oracle_addr: this.oracleAddr.toString(),
+    };
+  }
+
+  static fromMoveStruct(obj: OracleHeartbeatParamsMoveStruct) {
+    return new OracleHeartbeat

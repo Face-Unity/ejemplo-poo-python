cf8c05e6895b5bd7dc158e7e159378f549595ccb
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-08-30 07:47:49 +0000

    Docs improved

--- javascript/aptos.js/src/generated/types/OracleQueueSetConfigsParams.ts
+++ javascript/aptos.js/src/generated/types/OracleQueueSetConfigsParams.ts
@@ -1,4 +1,22 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import BN from "bn.js"; // eslint-disable-line @typescr
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IOracleQueueSetConfigsParams {
+  addr: HexString;
+  name: Uint8Array;
+  metadata: Uint8Array;
+  authority: HexString;
+  oracleTimeout: BN;
+  reward: BN;
+  minStake: BN;
+  slashingEnabled: boolean;
+  varianceToleranceMultiplier: types.SwitchboardDecimal;
+  feedProbationPeriod: BN;
+  consecutiveFeedFailureLimit: BN;
+  consecutiveOracleFailureLimit: BN;
+  unpermissionedFeedsEnabled: boolean;
+  lockLeaseFunding: boolean;
+  maxSize: BN;
+  saveConfirmationReward:

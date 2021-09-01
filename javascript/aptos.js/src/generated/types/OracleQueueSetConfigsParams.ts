6c277e1ebdcddf7a067218ba7b355257ac4fd468
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-09-01 09:43:27 +0000

    I18n: Update translation eu (100%).

--- javascript/aptos.js/src/generated/types/OracleQueueSetConfigsParams.ts
+++ javascript/aptos.js/src/generated/types/OracleQueueSetConfigsParams.ts
@@ -19,4 +19,44 @@ export interface IOracleQueueSetConfigsParams {
   unpermissionedFeedsEnabled: boolean;
   lockLeaseFunding: boolean;
   maxSize: BN;
-  saveConfirmationReward:
+  saveConfirmationReward: BN;
+  saveReward: BN;
+  openRoundReward: BN;
+  slashingPenalty: BN;
+}
+
+export interface OracleQueueSetConfigsParamsJSON {
+  addr: string;
+  name: Array<number>;
+  metadata: Array<number>;
+  authority: string;
+  oracleTimeout: string;
+  reward: string;
+  minStake: string;
+  slashingEnabled: boolean;
+  varianceToleranceMultiplier: types.SwitchboardDecimalJSON;
+  feedProbationPeriod: string;
+  consecutiveFeedFailureLimit: string;
+  consecutiveOracleFailureLimit: string;
+  unpermissionedFeedsEnabled: boolean;
+  lockLeaseFunding: boolean;
+  maxSize: string;
+  saveConfirmationReward: string;
+  saveReward: string;
+  openRoundReward: string;
+  slashingPenalty: string;
+}
+
+export interface OracleQueueSetConfigsParamsMoveStruct {
+  addr: string;
+  name: string;
+  metadata: string;
+  authority: string;
+  oracle_timeout: string;
+  reward: string;
+  min_stake: string;
+  slashing_enabled: boolean;
+  variance_tolerance_multiplier: types.SwitchboardDecimalMoveStruct;
+  feed_probation_period: string;
+  consecutive_feed_failure_limit: string;
+  consecutive_oracle_failure

86202c8ac7d96b6902b10c4edd62ca6f91a1ee41
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-09-17 21:01:53 +0000

    Clean

--- javascript/aptos.js/src/generated/types/OracleSlashEvent.ts
+++ javascript/aptos.js/src/generated/types/OracleSlashEvent.ts
@@ -1,3 +1,22 @@
 import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-import {
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IOracleSlashEvent {
+  aggregatorAddress: HexString;
+  oracleAddress: HexString;
+  amount: BN;
+  timestamp: BN;
+}
+
+export interface OracleSlashEventJSON {
+  aggregatorAddress: string;
+  oracleAddress: string;
+  amount: string;
+  timestamp: string;
+}
+
+export interface OracleSlashEventMoveStruct {
+  aggregator_address: string;
+  oracle_ad

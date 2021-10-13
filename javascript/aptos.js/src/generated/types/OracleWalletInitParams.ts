d414ea9bd81c119e2522d92a258b9c948415a76e
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-10-13 05:52:58 +0000

    update component

--- javascript/aptos.js/src/generated/types/OracleWalletInitParams.ts
+++ javascript/aptos.js/src/generated/types/OracleWalletInitParams.ts
@@ -6,3 +6,24 @@ import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 export interface IOracleWalletInitParams {
   oracleAddr: HexString;
   queueAddr: HexString;
+  withdrawAuthority: HexString;
+}
+
+export interface OracleWalletInitParamsJSON {
+  oracleAddr: string;
+  queueAddr: string;
+  withdrawAuthority: string;
+}
+
+export interface OracleWalletInitParamsMoveStruct {
+  oracle_addr: string;
+  queue_addr: string;
+  withdraw_authority: string;
+}
+
+export class OracleWalletInitParams implements IOracleWalletInitParams {
+  readonly oracleAddr: HexString;
+  readonly queueAddr: HexString;
+  readonly withdrawAuthority: HexString;
+
+  constructor(fields: IOracleWalletInitParams) 

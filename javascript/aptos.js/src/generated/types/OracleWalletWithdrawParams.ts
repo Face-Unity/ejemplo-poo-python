711b922ebafaaca67313c476623f1c4f0756ad4f
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-10-22 08:38:27 +0000

    fix javadoc warnings

--- javascript/aptos.js/src/generated/types/OracleWalletWithdrawParams.ts
+++ javascript/aptos.js/src/generated/types/OracleWalletWithdrawParams.ts
@@ -5,4 +5,25 @@ import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export interface IOracleWalletWithdrawParams {
   oracleAddr: HexString;
-  queueAddr: Hex
+  queueAddr: HexString;
+  amount: BN;
+}
+
+export interface OracleWalletWithdrawParamsJSON {
+  oracleAddr: string;
+  queueAddr: string;
+  amount: string;
+}
+
+export interface OracleWalletWithdrawParamsMoveStruct {
+  oracle_addr: string;
+  queue_addr: string;
+  amount: string;
+}
+
+export class OracleWalletWithdrawParams implements IOracleWalletWithdrawParams {
+  readonly oracleAddr: HexString;
+  readonly queueAddr: HexString;
+  readonly amount: BN;
+
+  constructor(fields: IOracleW

d05ae37c96e0af5ae691e839650333e567e2c0c7
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-11-05 16:11:54 +0000

    Minor wording changes.

--- javascript/aptos.js/src/generated/types/PermissionInitParams.ts
+++ javascript/aptos.js/src/generated/types/PermissionInitParams.ts
@@ -4,4 +4,27 @@ import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export interface IPermissionInitParams {
-  authority: HexS
+  authority: HexString;
+  granter: HexString;
+  grantee: HexString;
+}
+
+export interface PermissionInitParamsJSON {
+  authority: string;
+  granter: string;
+  grantee: string;
+}
+
+export interface PermissionInitParamsMoveStruct {
+  authority: string;
+  granter: string;
+  grantee: string;
+}
+
+export class PermissionInitParams implements IPermissionInitParams {
+  readonly authority: HexString;
+  readonly granter: HexString;
+  readonly grantee: HexString;
+
+  constructor(fields: IPermissionInitParams) {
+    this.authority = fields.

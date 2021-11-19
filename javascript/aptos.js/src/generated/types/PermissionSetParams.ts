5cff704f1c0accb91cdc1dbcd1e4c1eb878987e4
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-11-19 15:33:22 +0000

    delete old code

--- javascript/aptos.js/src/generated/types/PermissionSetParams.ts
+++ javascript/aptos.js/src/generated/types/PermissionSetParams.ts
@@ -1,1 +1,22 @@
-import * as types from "./"; // eslint-disable-line @types
+import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export interface IPermissionSetParams {
+  authority: HexString;
+  granter: HexString;
+  grantee: HexString;
+  permission: BN;
+  enable: boolean;
+}
+
+export interface PermissionSetParamsJSON {
+  authority: string;
+  granter: string;
+  grantee: string;
+  permission: string;
+  enable: boolean;
+}
+
+exp

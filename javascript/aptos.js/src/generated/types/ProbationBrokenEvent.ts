98955bfb834b4fdcdcb90552dd2dad544436f51d
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-11-26 05:50:17 +0000

    Update POT file.

--- javascript/aptos.js/src/generated/types/ProbationBrokenEvent.ts
+++ javascript/aptos.js/src/generated/types/ProbationBrokenEvent.ts
@@ -3,4 +3,27 @@ import * as types from "./"; // eslint-disable-line @typescript-eslint/no-unused
 import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-unused-vars
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-export interface IPr
+export interface IProbationBrokenEvent {
+  aggregatorAddress: HexString;
+  queueAddress: HexString;
+  timestamp: BN;
+}
+
+export interface ProbationBrokenEventJSON {
+  aggregatorAddress: string;
+  queueAddress: string;
+  timestamp: string;
+}
+
+export interface ProbationBrokenEventMoveStruct {
+  aggregator_address: string;
+  queue_address: string;
+  timestamp: string;
+}
+
+export class ProbationBrokenEvent implements IProbationBrokenEvent {
+  readonly aggregatorAddress: HexString;
+  readonly queueAddress: HexString;
+  readonly timestamp: BN;
+
+  constructor(fields:

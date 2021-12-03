6a9b92153a7ed279342cada3d80451e4779ca5bd
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-12-03 18:32:07 +0000

    fix #17

--- javascript/aptos.js/src/generated/types/SwitchboardDecimal.ts
+++ javascript/aptos.js/src/generated/types/SwitchboardDecimal.ts
@@ -4,4 +4,30 @@ import { HexString } from "aptos"; // eslint-disable-line @typescript-eslint/no-
 import Big from "big.js";
 import BN from "bn.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
 
-export interface ISwitchboar
+export interface ISwitchboardDecimal {
+  value: BN;
+  dec: number;
+  neg: boolean;
+}
+
+export interface SwitchboardDecimalJSON {
+  value: string;
+  dec: number;
+  neg: boolean;
+}
+
+export interface SwitchboardDecimalMoveStruct {
+  value: string;
+  dec: number;
+  neg: boolean;
+}
+
+export class SwitchboardDecimal implements ISwitchboardDecimal {
+  readonly value: BN;
+  readonly dec: number;
+  readonly neg: boolean;
+
+  constructor(fields: ISwitchboardDecimal) {
+    this.value = fields.value;
+    this.dec = fields.dec;
+  

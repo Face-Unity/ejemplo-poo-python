b0a1c2830f393ea7264fb98bc6f842c2be8ab0f1
Face-Unity <107359652+Face-Unity@users.noreply.github.com>
2021-12-13 21:13:35 +0000

    stable: update references to older 2.6 versions for 3.x

--- javascript/aptos.js/tests/decimal.tests.ts
+++ javascript/aptos.js/tests/decimal.tests.ts
@@ -17,4 +17,25 @@ describe("Decimal tests", () => {
     std_deviation: sbv2Decimal_100,
     min_response: sbv2Decimal_100,
     max_response: sbv2Decimal_100,
-    oracle_keys: ["", 
+    oracle_keys: ["", ""],
+    medians: [
+      {
+        vec: [sbv2Decimal_100],
+      },
+      {
+        vec: [sbv2Decimal_100],
+      },
+    ],
+    current_payout: [sbv2Decimal_100, sbv2Decimal_100],
+    errors_fulfilled: [false, false],
+    num_success: "",
+    num_error: "",
+    is_closed: true,
+    round_confirmed_timestamp: "",
+  };
+
+  it("Converts a SwitchboardDecimal", async () => {
+    const parsedDecimal =
+      sbv2.types.SwitchboardDecimal.fromMoveStruct(sbv2Decimal_100);
+
+    console.log(parsedDecimal);
